3e5cce4b04b5d19ad463459f6bc8952bf1bdc7d8
Fayel MOHAMED <fayel.mohamed@roborative.com>
2025-04-11 16:08:58 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,9 @@ use rust_file_master::Compression;
 
 
 fn main() -> anyhow::Result<()> {
-    let file_name = "./test/example2.txt".to_string();7
-    let file_ops = rust_file_master::FileOps::new(file_name.clone());
-    file_ops.create_file()?;
-    file_ops.write_file("Hello, world!")?;
+    let file_name = "./test/example2.txt".to_string();
     let compression = Compression::new(file_name.clone());
-    compression.
+    compression.decompress_zip()?;
+
     Ok(())
 }
